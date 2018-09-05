eca542eade3e97a166ccfc2ae273ad7c657810bf
iNickNing <228919961@qq.com>
2018-09-05 18:09:30 +0800

    add

--- src/main/java/top/inick/start/StartApplication.java
+++ src/main/java/top/inick/start/StartApplication.java
@@ -3,10 +3,15 @@ package top.inick.start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class StartApplication {
 
 	public static void main(String[] args) {
+		log.info("开始运行");
 		SpringApplication.run(StartApplication.class, args);
+		log.info("启动成功");
 	}
 }
---
+++ src/main/java/top/inick/start/domain/Demo.java
@@ -0,0 +1,15 @@
+package top.inick.start.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Demo {
+	private String code;
+	private String name;
+}
