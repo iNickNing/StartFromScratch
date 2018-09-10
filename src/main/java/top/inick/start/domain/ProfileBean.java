e6e53b8efa2d19a6855e74b00a6c3e8035159f1d
iNickNing <228919961@qq.com>
2018-09-10 09:43:10 +0800

    :construction:环境设置改变

---
+++ src/main/java/top/inick/start/domain/ProfileBean.java
@@ -0,0 +1,19 @@
+package top.inick.start.domain;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Profile("dev")//支持数组:@Profile({"dev","test"})
+@Configuration
+@Slf4j
+public class ProfileBean {
+	
+	@PostConstruct
+	public void init() {
+		log.info("dev 环境啊啊啊");
+	}
+}
