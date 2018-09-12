add71e4971f37ac895517e742b87662c2a6db0dd
iNickNing <228919961@qq.com>
2018-09-12 11:24:24 +0800

    :construction:拦截器的运用

--- src/main/java/top/inick/start/StartApplication.java
+++ src/main/java/top/inick/start/StartApplication.java
@@ -2,11 +2,13 @@ package top.inick.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import lombok.extern.slf4j.Slf4j;
 import top.inick.start.domain.Demo;
 
 @SpringBootApplication
+@ServletComponentScan
 @Slf4j
 public class StartApplication {
 
---
+++ src/main/java/top/inick/start/filters/CustomFilter.java
@@ -0,0 +1,41 @@
+package top.inick.start.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.springframework.core.annotation.Order;
+
+import lombok.extern.slf4j.Slf4j;
+
+//注册名称为customFilter的拦截器,拦截所有的请求
+@WebFilter(filterName="customFilter", urlPatterns= {"/*"})
+@Order(2)
+@Slf4j
+public class CustomFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		log.info("filter 初始化");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		log.info("doFilter 处理请求");
+		
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		log.info("filter 销毁");
+	}
+
+}
